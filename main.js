c294c6505bfff3f05934c370e676fd62ddddcc24
Walid Rashad <waleed1600@hotmail.com>
2025-09-13 18:15:23 +0300

    update

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-window.onload = function() { 
 
-    Document.querySelector("h1").style.color = "red";
 
-}
+let name = "John";
+let age = 30;
+let isStudent = true;
+let hobbies = ["reading", "traveling", "swimming"];
+let address = { city: "New York", country: "USA" };
 
-window.alert("Hello, World!");
-document.write("<h1>Hello, World!</h1>");
+let personCard = 
+<div id = "card">
 
-console.log("Hello, World!");
-console.error("This is an error message");
-var name = "Alice";
-console.log('Hello, World! $name');
-hello.innerHTML = "open";
-
+<div id = "name"> Name: ${name} </div>
+<div id = "age"> Age: ${age} </div>
+<div id = "isStudent"> Student: ${isStudent} </div>
+<div id = "hobbies"> Hobbies: ${hobbies.join(", ")} </div>
+<div id = "address"> Address: ${address.city}, ${address.country} </div> </div>
